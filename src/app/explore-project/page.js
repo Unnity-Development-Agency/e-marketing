1f18fceab2dc0eaa52e989a9902f5d7ee45d68db
surajjha7428 <sjha74280@gmail.com>
2026-06-11 16:18:15 +0530

    Don Miller and Polki Saree

--- src/app/explore-project/page.js
+++ src/app/explore-project/page.js
@@ -25,6 +25,18 @@ export default function CaseStudies() {
       href: "https://cupidclothings.com/",
     },
     {
+      id: 18,
+      title: "Don Miller",
+      image: "/don-millerr.jpeg",
+      href: "https://www.donmiller.in/",
+    },
+    {
+      id: 19,
+      title: "Polki Sarees",
+      image: "/polki-saree.jpeg",
+      href: "https://polkisarees.com/",
+    },
+    {
       id: 3,
       title: "Uberlyfe",
       image: "/uber.png",
@@ -117,18 +129,7 @@ export default function CaseStudies() {
       image: "/prateek-suri.png",
       href: "https://author-rose.vercel.app/",
     },
-    {
-      id: 18,
-      title: "Don Miller",
-      image: "/don-millerr.jpeg",
-      href: "https://www.donmiller.in/",
-    },
-    {
-      id: 19,
-      title: "Polki Sarees",
-      image: "/polki-saree.jpeg",
-      href: "https://polkisarees.com/",
-    },
+    
   ];
   useEffect(() => {
      document.title = "Our Projects | Websites, Funnels & Ad Campaign";
